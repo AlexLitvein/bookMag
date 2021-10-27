29ae72854c88976c1b93b71ba9e5b6b7dce15c20
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-10-27 15:42:09 +0700

    marker circle

--- src/react_comp/SvgChart.js
+++ src/react_comp/SvgChart.js
@@ -189,10 +189,13 @@ const SvgChart = ({ options, axis, dataSets = [] }) => {
             {console.log('draw SvgChart')}
 
             {/* TODO: add marker by color data */}
+            {/* TODO: маркер не изменяет размер при изм размера его используещего */}
             <defs>
-                <marker id="mrkVHAxis" className="mrk" markerWidth="1" markerHeight="6" refX="0.5" refY="6"
+                <marker id="mrkVHAxis" className="mrk" markerWidth="10" markerHeight="10" refX="5" refY="5"
                     orient="auto">
-                    <line x1="0" y1="0" x2="0" y2="6" />
+                    {/* <line x1="0" y1="0" x2="0" y2="6" /> */}
+                    {/* <line x2="0" y2="6" /> */}
+                    <circle cx="5" cy="5" r="5" />
                 </marker>
             </defs>
 
